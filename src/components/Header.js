3f4d32e2f5b75308754f656c7ed11a371ae7a3c7
Christine Benedict <learn@learn08.localdomain>
2018-05-17 13:21:41 -0700

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Header } from 'react-bootstrap'
+
+class Header extends Component{
+  render(){
+    return(
+      <Header>
+        Cat Tinder
+      </Header>
+    )
+  }
+}
+
+export default Header
