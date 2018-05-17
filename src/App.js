51ae2bcd83331b0d89324a7231bf43fba0ba6784
Christine Benedict <learn@learn08.localdomain>
2018-05-17 13:19:13 -0700

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from react-router-dom'
+import Cats from ./pages/Cats
+import NewCat from ./pages/NewCat
+import Header from ./components/Header
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/cats" component={Cats} />
+            <Route exact path="/" component={NewCat} />
+          </Switch>
+        </Router>
       </div>
     );
   }
